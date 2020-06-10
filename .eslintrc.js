6e0b13b220cb8e03d7d1b5773d3df0ed0a8db585
jaxx2104 <jaxx2104@gmail.com>
2020-06-10 18:49:01 +0900

    :sparkles: Support typescript lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,31 @@
 module.exports = {
-  parser: "babel-eslint",
-  extends: ["prettier", "prettier/react"],
-  plugins: ["react", "prettier"],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "prettier/react",
+  ],
+  plugins: ["@typescript-eslint", "react", "react-hooks", "prettier"],
   parserOptions: {
-    ecmaVersion: 2016,
+    ecmaVersion: 2018,
     sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
   },
-  env: {
-    es6: true,
-    node: true,
-  },
   rules: {
     "prettier/prettier": "error",
+  },
+  settings: {
+    react: {
+      version: "16.13.0",
+    },
   },
 }
