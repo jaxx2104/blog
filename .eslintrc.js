fe3c841f09234091098f2f6b9396e06cbfccadfc
jaxx2104 <jaxx2104@gmail.com>
2020-06-21 15:49:30 +0900

    :package: typescript-eslint を追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,36 @@
 module.exports = {
-  parser: "babel-eslint",
-  extends: ["prettier", "prettier/react"],
-  plugins: ["react", "prettier"],
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "prettier/react"
+  ],
+  plugins: ["@typescript-eslint", "react", "import", "react-hooks", "prettier"],
   parserOptions: {
-    ecmaVersion: 2016,
+    ecmaVersion: 2018,
     sourceType: "module",
     ecmaFeatures: {
       jsx: true
     }
   },
-  env: {
-    es6: true,
-    node: true
-  },
   rules: {
-    "prettier/prettier": "error"
+    "prettier/prettier": "error",
+    "import/order": "error",
+    "@typescript-eslint/explicit-function-return-type": "off"
+  },
+  settings: {
+    react: {
+      version: "16.13.0"
+    }
   }
 }
