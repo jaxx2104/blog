696adbf553c66c12845d4194762bb484c5b16bbf
jaxx2104 <jaxx2104@gmail.com>
2026-05-02 00:57:14 +0900

    refactor(ui/badge): swap styled-components to css module with data-primary

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -1,46 +1,26 @@
 import type React from "react"
-import styled from "styled-components"
+import styles from "./badge.module.css"
 
 interface Props {
   items: string[] | null
   primary?: boolean
 }
 
-const Badges: React.FC<Props> = ({ items, primary }: Props) => {
-  if (!items) {
-    return null
-  }
-
+const Badges: React.FC<Props> = ({ items, primary }) => {
+  if (!items) return null
   return (
     <>
-      {items.map((item, i) => {
-        return (
-          <Badge key={i} $primary={primary}>
-            {item}
-          </Badge>
-        )
-      })}
+      {items.map((item, i) => (
+        <span
+          key={i}
+          className={styles.badge}
+          data-primary={primary ? "" : undefined}
+        >
+          {item}
+        </span>
+      ))}
     </>
   )
 }
 
 export default Badges
-
-const Badge = styled.span<{ $primary?: boolean }>`
-  background-color: ${(props) =>
-    props.$primary ? props.theme.colorMain : props.theme.colorSub};
-  border-radius: 4px;
-  box-sizing: border-box;
-  color: rgb(255, 255, 255);
-  display: inline;
-  font-size: ${(props) => `${props.theme.fontSizeSmall}rem`};
-  line-height: ${(props) => `${props.theme.fontSizeSmall}rem`};
-  font-weight: ${(props) => props.theme.fontWeight};
-  outline-style: none;
-  margin: auto 0;
-  text-decoration-line: none;
-  padding: ${(props) =>
-    `${props.theme.fontSizeSmall / 2}rem ${props.theme.fontSizeSmall}rem`};
-  text-align: center;
-  white-space: nowrap;
-`
