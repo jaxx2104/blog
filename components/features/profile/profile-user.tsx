de2e4bc501e36fcee893cab3a66a7336a5094304
jaxx2104 <jaxx2104@gmail.com>
2026-05-02 23:40:09 +0900

    refactor(features/profile-user): swap styled-components to css module

--- components/features/profile/profile-user.tsx
+++ components/features/profile/profile-user.tsx
@@ -1,10 +1,10 @@
 import type React from "react"
-import styled from "styled-components"
 import Container from "../../ui/container"
 import Display from "../../ui/display"
 import Flex from "../../ui/flex"
 import Section from "../../ui/section"
 import Thumbnail from "./thumbnail"
+import styles from "./profile-user.module.css"
 
 interface Props {
   profileImage?: string
@@ -12,30 +12,24 @@ interface Props {
 
 const ProfileUser: React.FC<Props> = ({
   profileImage = "/images/profile.jpg",
-}: Props) => (
+}) => (
   <Section>
     <Container>
       <Flex>
-        <BioWrap>
+        <div>
           <Display>Futoshi Iwashita</Display>
           <strong>jaxx2104</strong>
           <p>I&apos;m a front-end engineer in Japan 🗼</p>
           <li>2013 ~ 2017: J-CAST</li>
           <li>2017 ~ 2020: Recruit</li>
           <li>2020 ~ : freee</li>
-        </BioWrap>
-        <UserWrap>
+        </div>
+        <div className={styles.user}>
           <Thumbnail src={profileImage} title="jaxx2104" size={160} circle />
-        </UserWrap>
+        </div>
       </Flex>
     </Container>
   </Section>
 )
 
 export default ProfileUser
-
-const UserWrap = styled.div`
-  margin: 3rem auto 2rem;
-`
-
-const BioWrap = styled.div``
